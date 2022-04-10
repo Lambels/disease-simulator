6e7ac4bf76aef2711573ccc183b21d7952eba8c2
Lambels <patrick.arvatu@yahoo.com>
2022-04-10 17:47:43 +0300

    finished patient implementation

--- patient.go
+++ patient.go
@@ -1,13 +1,17 @@
 package main
 
-import "sync"
+import (
+	"math"
+	"math/rand"
+	"sync"
+)
 
 type patient struct {
 	// maxInteractions is the max number of interactions the patient can have.
 	maxInteractions int
 
 	// interactionMu protects all the fields under.
-	interactionMu sync.RWMutex
+	interactionMu sync.Mutex
 	// interactedWith holds all the patients with which the patient has had contact
 	// with.
 	interactedWith []*patient
@@ -19,7 +23,7 @@ type patient struct {
 }
 
 func (p *patient) interact(with *patient) {
-	if !p.canInteract() {
+	if !p.canInteract(with) {
 		return
 	}
 
@@ -28,15 +32,36 @@ func (p *patient) interact(with *patient) {
 	p.interactionMu.Unlock()
 
 	if with.isInfected() {
+		if p.isInfected() {
+			with.interact(p)
+			return
+		}
 
+		p.infect()
 	}
+
+	// call interaction with next patient, interactions go both ways.
+	with.interact(p)
 }
 
 // isInfected is a conccurency safe way of accessing p.interactedWith.
-func (p *patient) canInteract() bool {
+func (p *patient) canInteract(with *patient) bool {
 	p.interactionMu.Lock()
 	defer p.interactionMu.Unlock()
-	return len(p.interactedWith) < p.maxInteractions
+
+	// patient cant interact no more.
+	if len(p.interactedWith) > p.maxInteractions {
+		return false
+	}
+
+	// search for (with) linearly.
+	for _, pat := range p.interactedWith {
+		if pat == with {
+			return false // alreay interacted.
+		}
+	}
+
+	return true
 }
 
 // isInfected is a conccurency safe way of accessing p.infected.
@@ -44,4 +69,13 @@ func (p *patient) isInfected() bool {
 	p.interactionMu.Lock()
 	defer p.interactionMu.Unlock()
 	return p.infected
+}
+
+func (p *patient) infect() {
+	prob := math.Floor(rand.Float64()*100) / 100
+	if float32(prob) < p.infectionChance {
+		p.interactionMu.Lock()
+		p.infected = true
+		p.interactionMu.Unlock()
+	}
 }
