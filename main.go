abf8126bce6a0c3b2b3aa7bcfd84de5bd190898e
Lambels <patrick.arvatu@yahoo.com>
2022-04-11 18:32:34 +0300

    refactor code

--- main.go
+++ main.go
@@ -13,8 +13,9 @@ var (
 	popMaxInteractionsFlag = flag.Int("population-max-interactions", 3, "population-max-interactions determines the max number of interactions a pacient can have")
 	infectionRateFlag      = flag.Float64("infection-rate", 0.5, "infection-rate determines the chance of disease transmition, ie: 0.5, 0.25, 1.0")
 
-	processGeneratorsFlag = flag.Int("process-generators", 1, "process-generators determines the number of generators used")
-	processProcessorsFlag = flag.Int("process-processors", 1, "process-processors determines the number of processors used")
+	processGeneratorsFlag       = flag.Int("process-generators", 1, "process-generators determines the number of generators used")
+	processProcessorsFlag       = flag.Int("process-processors", 1, "process-processors determines the number of processors used")
+	processProcessorTimeoutFlag = flag.Int("process-processor-timeout", 10, "process-processor-timeout determines the number of seconds used as processor timeout")
 )
 
 // generate generates 2 random pacients from population field: from.
@@ -38,16 +39,21 @@ func generate(from *population, to chan<- [2]*patient) {
 		for !pat2.canInteract(nil) {
 			from.RemoveKey(key)
 			pat2, key = from.Random()
+
+			if pat2 == nil { // no more patients.
+				return
+			}
 		}
 
 		to <- [2]*patient{pat1, pat2}
 	}
+
 }
 
-// simulateInteraction processes interactions from (from) with a 10 second timeout.
+// simulateInteraction processes interactions from (from).
 // run in own gorutine.
 func simulateInteraction(from <-chan [2]*patient) {
-	timer := time.NewTimer(10 * time.Second)
+	timer := time.NewTimer(time.Duration(*processProcessorTimeoutFlag) * time.Second)
 	for {
 		select {
 		case <-timer.C: // dormant for to long
@@ -55,10 +61,15 @@ func simulateInteraction(from <-chan [2]*patient) {
 			return
 
 		case intercourse := <-from:
-			intercourse[0].interact(intercourse[1])
+			time.Sleep(time.Second)
+			if err := intercourse[0].interact(intercourse[1]); err != nil {
+				break
+			}
+			time.Sleep(time.Second)
+			intercourse[1].interact(intercourse[0])
 		}
 		// after each process reset the timer.
-		timer.Reset(10 * time.Second)
+		timer.Reset(time.Duration(*processProcessorTimeoutFlag) * time.Second)
 	}
 }
 
